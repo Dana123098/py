07199c273c08dae72e86821acfef1ec64ac26fe4
Dana123098 <74066114+Dana123098@users.noreply.github.com>
2022-10-19 17:47:38 +0200

    Add files via upload

---
+++ DanaLab2/001.py
@@ -0,0 +1,40 @@
+# import random
+
+# user = input("Wybierz - paper, norzyce , kamień : ")
+
+# possible_actions=["paper","norzyce","kamień"]
+# computer_action = random.choice(possible_actions)
+# print(f"user:{user}computer:{computer_action}")
+# if user ==computer_action:
+#     print("obaj wybrali takie same {user_action}. Remis!")
+# elif user == "kamień":
+#     if computer_action=="norzyce":
+#         print("kamień bije nożyczki Wygrałeś!")
+#     else :
+#         print("papier owija kamień , przegrłeś")
+# elif user == "paper":
+#     if computer_action=="kamień":
+#          print("Wygrałeś!")
+# elif user == "norzyce":
+#     if computer_action=="kamień":
+#          print("przegrłeś")
+# elif user== "kamień":
+#     if computer_action=="paper":
+#          print("przegrłeś")
+# elif user== "paper":
+#     if computer_action=="norzyce":
+#          print("przegrłeś")
+# elif user== "norzyce":
+#     if computer_action=="paper":
+#          print("Wygrałeś!")
+    
+
+
+user=("wp. trzy dodatnie liczby")
+a = int(input("1:"))
+b = int(input("2:"))
+c = int(input("3:"))
+if a>b:
+    print(a)
+else
+
